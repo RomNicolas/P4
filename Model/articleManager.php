634f87301592a6c65f7267ef9e05d88551fe1e4f
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 17:18:14 +0200

    Function getArticles
    Création de la fonction getArticles pour récupérer la liste des billets via la classe ArticleManager

--- Model/articleManager.php
+++ Model/articleManager.php
@@ -1,3 +1,11 @@
 <?php
 
+namespace OpenClassrooms\Blog\Model;
 require_once("model/Manager.php");
+
+class PostManager extends Manager {
+	public function getPosts() {
+		$db = $this->dbConnect();
+		$req = $db->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles ORDER BY date_creation DESC LIMIT 0, 5');
+		return $req;
+	}
