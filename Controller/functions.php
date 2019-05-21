3ca9eb88327fcaaa782d315004dfa0e7d15cbade
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 19:28:17 +0200

    Controller listPosts
    Mise à jour de controller

--- Controller/functions.php
+++ Controller/functions.php
@@ -3,3 +3,9 @@
 // Chargement des classes
 require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
+
+function listPosts() {
+	$postManager = new \OpenClassrooms\Blog\Model\PostManager();
+	$posts = $postManager->getPosts();
+	require('view/frontend/listPostsView.php');
+}
