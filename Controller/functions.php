210a3bde3edb4750316a74a24b5907d86f5c4b7d
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 19:31:08 +0200

    controller article
    Mise à jour du controller de la liste des articles + création du controller d'un article

--- Controller/functions.php
+++ Controller/functions.php
@@ -4,8 +4,16 @@
 require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
 
-function listPosts() {
+function listArticles() {
 	$postManager = new \OpenClassrooms\Blog\Model\PostManager();
 	$posts = $postManager->getPosts();
-	require('view/frontend/listPostsView.php');
+	require('View/listArticlesView.php');
+}
+
+function post() {
+	$postManager = new \OpenClassrooms\Blog\Model\PostManager();
+	$commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+	$post = $postManager->getPost($_GET['id']);
+	$comments = $commentManager->getComments($_GET['id']);
+	require('View/articleView.php');
 }
