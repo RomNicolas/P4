e60f66d995355f9e3a8e25242a0c0a9be14d1615
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 17:41:45 +0200

    Function getComments
    Création de la classe CommentManager +
    Récupère la liste des commentaires sur un article précis

--- Model/commentManager.php
+++ Model/commentManager.php
@@ -1,3 +1,17 @@
 <?php
 
+namespace OpenClassrooms\Blog\Model;
 require_once("model/Manager.php");
+
+class CommentManager extends Manager
+{
+
+	//Recupère les commentaires d'un article
+    public function getComments($postId) {
+		$db = $this->dbConnect();
+		$comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE article_id = ? ORDER BY date_creation DESC');
+		$comments->execute(array($postId));
+		return $comments;
+	}
+
+}
