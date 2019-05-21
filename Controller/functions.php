2be0393b728a01b6b1d4d42d0752467b29ea6ea8
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 19:34:34 +0200

    controller addComment
    relie l'ajout de commentaires

--- Controller/functions.php
+++ Controller/functions.php
@@ -5,15 +5,26 @@ require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
 
 function listArticles() {
-	$postManager = new \OpenClassrooms\Blog\Model\PostManager();
+	$postManager = new \OpenClassrooms\Blog\Model\ArticleManager();
 	$posts = $postManager->getPosts();
 	require('View/listArticlesView.php');
 }
 
 function article() {
-	$postManager = new \OpenClassrooms\Blog\Model\PostManager();
+	$postManager = new \OpenClassrooms\Blog\Model\ArticleManager();
 	$commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
 	$post = $postManager->getPost($_GET['id']);
 	$comments = $commentManager->getComments($_GET['id']);
 	require('View/articleView.php');
+}
+
+function addComment($postId, $author, $comment) {
+	$commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+	$affectedLines = $commentManager->postComment($postId, $author, $comment);
+	if ($affectedLines === false) {
+		throw new Exception('Impossible d\'ajouter le commentaire !');
+	}
+	else {
+		header('Location: index.php?action=post&id=' . $postId);
+	}
 }
