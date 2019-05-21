9bbd98cf74b4e34b0a669735375f21e7640bbd2d
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 17:23:00 +0200

    Function getArticle
    Récupérer 1 article parmi le reste

--- Model/articleManager.php
+++ Model/articleManager.php
@@ -3,9 +3,18 @@
 namespace OpenClassrooms\Blog\Model;
 require_once("model/Manager.php");
 
-class PostManager extends Manager {
-	public function getPosts() {
+class ArticleManager extends Manager {
+
+	public function getArticles() {
 		$db = $this->dbConnect();
 		$req = $db->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles ORDER BY date_creation DESC LIMIT 0, 5');
 		return $req;
+	}
+
+	public function getArticle($postId) {
+		$db = $this->dbConnect();
+		$req = $db->prepare('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles WHERE id = ?');
+		$req->execute(array($postId));
+		$post = $req->fetch();
+		return $post;
 	}
