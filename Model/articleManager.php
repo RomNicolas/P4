114ca5c6d9281c2eb3662c94ed4c9257a9d48908
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 17:38:13 +0200

    Other possibility of functions
    Création de fonctions vides mais qui vont se remplir à l'avenir

--- Model/articleManager.php
+++ Model/articleManager.php
@@ -5,12 +5,14 @@ require_once("model/Manager.php");
 
 class ArticleManager extends Manager {
 
+	//Recupère la liste des articles
 	public function getArticles() {
 		$db = $this->dbConnect();
 		$req = $db->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles ORDER BY date_creation DESC LIMIT 0, 5');
 		return $req;
 	}
 
+	//Recupère un article
 	public function getArticle($postId) {
 		$db = $this->dbConnect();
 		$req = $db->prepare('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles WHERE id = ?');
@@ -18,3 +20,25 @@ class ArticleManager extends Manager {
 		$post = $req->fetch();
 		return $post;
 	}
+
+	//Recupère un article à modifier
+	public function modifyArticle() {
+		//SELECT
+	}
+
+	//Permet la création d'un article
+	public function createArticle() {
+		//INSERT
+	}
+
+	//Confirme la modification de l'article
+	public function validateModifyArticle() {
+		//UPDATE
+	}
+
+	//Confirme la suppression de l'article
+	public function deleteArticle() {
+		//DELETE
+	}
+
+}
