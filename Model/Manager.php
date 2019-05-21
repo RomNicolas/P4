aeb2d7227766183daf19b6e15b74e427c44a5790
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-05-21 10:23:33 +0200

    Création du dossier Model
    Permet les différents accès à la BDD

---
+++ Model/Manager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OpenClassrooms\Blog\Model;
+
+class Manager
+{
+    protected function dbConnect()
+    {
+        $db = new \PDO('mysql:host=localhost;dbname=myblog;charset=utf8', 'root', '');
+        return $db;
+    }
+}
