909a71244a34dbc5fae92ee98c4aa96e26b29cc8
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-06-03 19:32:03 +0200

    footer

---
+++ View/footer.php
@@ -0,0 +1,6 @@
+<?php ob_start(); ?>
+	<p>Jean Forteroche</p>
+	<p><a href="twitter.php">Twitter</a></p>
+	<p><a href="facebook.php">Facebook</a></p>
+	<p><a href="insta.php">Instagram</a></p>
+<?php $footer = ob_get_clean(); ?>
