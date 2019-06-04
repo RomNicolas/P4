346b43673c8d25f7f8dc7aa984b5bfb690d0ef92
RomNicolas <46890514+RomNicolas@users.noreply.github.com>
2019-06-04 22:08:55 +0200

    upgrade modifyArticle / createArticle
    Permet le stockage de la modification et de la création d'un article dans la bdd

--- Model/articleManager.php
+++ Model/articleManager.php
@@ -5,14 +5,14 @@ require_once("model/Manager.php");
 
 class ArticleManager extends Manager {
 
-	//Recupère la liste des articles
+	//Récupère la liste des articles
 	public function getArticles() {
 		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles ORDER BY date_creation DESC LIMIT 0, 5');
+		$req = $db->query('SELECT id, title, SUBSTRING(content,1,500) AS content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles ORDER BY date_creation DESC');
 		return $req;
 	}
 
-	//Recupère un article
+	//Récupère un article
 	public function getArticle($postId) {
 		$db = $this->dbConnect();
 		$req = $db->prepare('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles WHERE id = ?');
@@ -21,24 +21,36 @@ class ArticleManager extends Manager {
 		return $post;
 	}
 
-	//Recupère un article à modifier
-	public function modifyArticle() {
-		//SELECT
-	}
+	//Récupère un article à modifier
+	public function modifyArticle($id) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT * FROM articles WHERE id = ?');
+        $req->execute(array($id));
+        return $req;
+    }
 
 	//Permet la création d'un article
 	public function createArticle() {
-		//INSERT
+		$bd = $this->dbConnect();
+		$req = $bd->prepare('INSERT INTO articles (author, title, content, date_creation, date_modification) VALUES ("Jean Forteroche", :title, :content, NOW(), NOW())');
+		$req->execute(array('title' => $_POST['title'], 'content' => $_POST['createtextarea'] ));
+        return $req;
 	}
 
 	//Confirme la modification de l'article
-	public function validateModifyArticle() {
-		//UPDATE
-	}
+    public function valideModifArticle($id) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE articles SET title = :title, content = :content, date_modification = NOW() WHERE id = :id');
+        $req->execute(array('title' => $_POST['title'], 'content' => $_POST['mytextarea'], 'id' => $_GET['id'] ));
+        return $req;
+    }
 
 	//Confirme la suppression de l'article
-	public function deleteArticle() {
-		//DELETE
+	public function deleteArticle($id) {	
+		$db = $this->dbConnect();
+		$req = $db->prepare('DELETE FROM articles WHERE id = ?');
+		$deleteArticle = $req->execute(array($id));
+		return $deleteArticle;
 	}
 
 }
